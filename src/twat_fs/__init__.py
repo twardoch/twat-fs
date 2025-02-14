646acb5cd052fcb63ac5e27b569eea8c4e138636
twardoch <adam@twardoch.com>
2025-02-14 19:46:19 +0100

    Initial commit

---
+++ src/twat_fs/__init__.py
@@ -0,0 +1,5 @@
+"""twat fs plugin"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__name__) 
---
+++ tests/test_package.py
@@ -0,0 +1,6 @@
+"""Test suite for twat_fs."""
+
+def test_version():
+    """Verify package exposes version."""
+    import twat_fs
+    assert twat_fs.__version__
